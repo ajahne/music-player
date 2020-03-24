3c33d6c5496530d548085e4e9a07f18eaefeeaf3
ajahne <ajahne@gmail.com>
2020-03-24 16:32:58 -0400

    inlined template code from template.js

--- src/vjs/index.js
+++ src/vjs/index.js
@@ -64,9 +64,27 @@ function audioPlayer() {
   }
 
   function createMusicPlayer() {
-    let template = getTemplate();
+    function getTemplate() {
+        const template = `<div id="music-player">
+        <div class="wrapper">
+          <img class="image">
+          <div class="song-info">
+            <div class="song-title"></div>
+            <div class="song-artist"></div>
+          </div>
+          <div class="play-button is-paused"></div>
+          <div class="next-button"></div>
+        </div>
+        <div class='progress'>
+          <div class='bar'></div>
+        </div>
+      </div>`;
+
+      return template;
+    }
+    
     musicPlayer = document.getElementById('music-player');
-    musicPlayer.innerHTML = template;
+    musicPlayer.innerHTML = getTemplate();
   }
 
   function createMusicPlayerElements() {
